e7865f810a7fa7b60859502686d75ebef051c3c5
amy-jung <amy@sharedrealities.co>
2018-02-18 03:53:21 -0700

    add table
    Added light theme table with Material UI.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import 'normalize.css'
 import './styles/main.css'
 import Header from './components/layout/Header'
 import SideNav from './components/layout/SideNav'
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+
 
 class App extends Component {
   render() {
     const showSideNav = this.props.location.pathname !== '/' && this.props.location.pathname !== '/login'
     return (
-      <div className="App">
-        <Header />
-        {/* { showSideNav && <SideNav /> } */}
-        <div className='side-nav-wrapper'>
-          <SideNav />
+      <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+        <div className="App">
+          <Header />
+          {/* { showSideNav && <SideNav /> } */}
+          <div className='side-nav-wrapper'>
+            <SideNav />
+          </div>
+          <div className='main-wrapper'>{ this.props.children }</div>
         </div>
-        <div className='main-wrapper'>{ this.props.children }</div>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
