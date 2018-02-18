5d165472dcc26625986bbc81510456b79e40520a
amy-jung <amy@sharedrealities.co>
2018-02-18 06:23:44 -0700

    Updated Inbox Table
    Added toggles, got rid of checkboxes in inbox table

--- src/pages/Auth/Inbox.js
+++ src/pages/Auth/Inbox.js
@@ -7,6 +7,12 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
+import Toggle from 'material-ui/Toggle';
+const styles = {
+  toggle: {
+    marginBottom: 16,
+  },
+};
 
 
 export default () => {
@@ -17,38 +23,55 @@ export default () => {
       </div>
       <div className="inbox-table">
         <Table>
-          <TableHeader>
+          <TableHeader displaySelectAll={false}
+            adjustForCheckbox={false}>
             <TableRow>
-              <TableHeaderColumn>ID</TableHeaderColumn>
-              <TableHeaderColumn>Name</TableHeaderColumn>
-              <TableHeaderColumn>Status</TableHeaderColumn>
+              <TableHeaderColumn>Approve?</TableHeaderColumn>
+              <TableHeaderColumn>Requested From</TableHeaderColumn>
+              <TableHeaderColumn>Proposed Context</TableHeaderColumn>
+              <TableHeaderColumn>Time Requested</TableHeaderColumn>
             </TableRow>
           </TableHeader>
-          <TableBody>
+          <TableBody displayRowCheckbox={false}>
             <TableRow>
+              <TableRowColumn> <Toggle
+                style={styles.toggle} />
+              </TableRowColumn>
               <TableRowColumn>1</TableRowColumn>
               <TableRowColumn>John Smith</TableRowColumn>
               <TableRowColumn>Employed</TableRowColumn>
             </TableRow>
             <TableRow>
-              <TableRowColumn>2</TableRowColumn>
-              <TableRowColumn>Randal White</TableRowColumn>
-              <TableRowColumn>Unemployed</TableRowColumn>
+              <TableRowColumn> <Toggle
+                style={styles.toggle} />
+                <TableRowColumn>2</TableRowColumn>
+                <TableRowColumn>Randal White</TableRowColumn>
+                <TableRowColumn>Unemployed</TableRowColumn>
+              </TableRowColumn>
             </TableRow>
             <TableRow>
-              <TableRowColumn>3</TableRowColumn>
-              <TableRowColumn>Stephanie Sanders</TableRowColumn>
-              <TableRowColumn>Employed</TableRowColumn>
+              <TableRowColumn><Toggle
+                style={styles.toggle} />
+                <TableRowColumn>3</TableRowColumn>
+                <TableRowColumn>Stephanie Sanders</TableRowColumn>
+                <TableRowColumn>Employed</TableRowColumn>
+              </TableRowColumn>
             </TableRow>
             <TableRow>
-              <TableRowColumn>4</TableRowColumn>
-              <TableRowColumn>Steve Brown</TableRowColumn>
-              <TableRowColumn>Employed</TableRowColumn>
+              <TableRowColumn><Toggle
+                style={styles.toggle} />
+                <TableRowColumn>4</TableRowColumn>
+                <TableRowColumn>Steve Brown</TableRowColumn>
+                <TableRowColumn>Employed</TableRowColumn>
+              </TableRowColumn>
             </TableRow>
             <TableRow>
-              <TableRowColumn>5</TableRowColumn>
-              <TableRowColumn>Christopher Nolan</TableRowColumn>
-              <TableRowColumn>Unemployed</TableRowColumn>
+              <TableRowColumn> <Toggle
+                style={styles.toggle} />
+                <TableRowColumn>5</TableRowColumn>
+                <TableRowColumn>Christopher Nolan</TableRowColumn>
+                <TableRowColumn>Unemployed</TableRowColumn>
+              </TableRowColumn>
             </TableRow>
           </TableBody>
         </Table>
